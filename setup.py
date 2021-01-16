dbc5453d257ec50dc3d5e82a3c4e04a19b8377fb
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-01-16 21:30:11 +0100

    bump python requirement to 3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 
 # Give setuptools a hint to complain if it's too old a version
-SETUP_REQUIRES = ["setuptools >= 46.4.0", "numpy"]
+SETUP_REQUIRES = ["setuptools >= 46.4.0"]
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
