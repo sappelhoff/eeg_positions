c7e16b2a75a546d8ed723aaec5b3f0debc8b32d6
Clemens Brunner <clemens.brunner@gmail.com>
2022-06-15 10:51:18 +0200

    Some style fixes

--- examples/plot_positions.py
+++ examples/plot_positions.py
@@ -12,14 +12,16 @@ Plot electrode positions
 from eeg_positions import get_elec_coords, plot_coords
 
 # %%
-# Let's start with the basic 10-20 system in two dimensions (``"x"`` and ``"y"``).
+# Let's start with the basic 10-20 system in two dimensions:
 
 coords = get_elec_coords(
     system="1020",
     dim="2d",
 )
 
-# This function returns `coords`, which is a pandas.DataFrame object:
+# %%
+# This function returns a ``pandas.DataFrame`` object:
+
 coords.head()
 
 # %%
@@ -28,14 +30,14 @@ coords.head()
 # the color of the electrodes and the text annotations.
 
 fig, ax = plot_coords(
-    coords, scatter_kwargs=dict(color="green"), text_kwargs=dict(fontsize=10)
+    coords, scatter_kwargs=dict(color="g"), text_kwargs=dict(fontsize=10)
 )
 
 fig
 
 # %%
-# Notice that the "landmarks" ``NAS``, ``LPA``, and ``RPA`` are present. We can drop
-# these by passing the ``drop_landmarks=True`` to :func:`get_elec_coords`:
+# Notice that the "landmarks" ``NAS``, ``LPA``, and ``RPA`` are included. We can drop
+# these by passing ``drop_landmarks=True`` to :func:`get_elec_coords`:
 
 coords = get_elec_coords(
     system="1020",
@@ -44,7 +46,7 @@ coords = get_elec_coords(
 )
 
 fig, ax = plot_coords(
-    coords, scatter_kwargs=dict(color="green"), text_kwargs=dict(fontsize=10)
+    coords, scatter_kwargs=dict(color="g"), text_kwargs=dict(fontsize=10)
 )
 
 fig
@@ -54,9 +56,10 @@ fig
 # example, let's assume we have the following 64 channel labels (based on the 10-05
 # system):
 
-chans = """Fp1 AF7 AF3 F1 F3 F5 F7 Fp2 AF8 AF4 F2 F4 F6 F8 FT7 FC5 FC3 FC1 C1 C3 C5 T7
-TP7 CP5 CP3 CP1 FT8 FC6 FC4 FC2 C2 C4 C6 T8 TP8 CP6 CP4 CP2 P1 P3 P5 P7 P9 PO7 PO3 O1 P2
-P4 P6 P8 P10 PO8 PO4 O2 Iz Oz POz Pz Fz AFz Fpz CPz Cz FCz""".split()
+chans = """Fp1 AF7 AF3 F1 F3 F5 F7 Fp2 AF8 AF4 F2 F4 F6 F8 FT7 FC5 FC3
+FC1 C1 C3 C5 T7 TP7 CP5 CP3 CP1 FT8 FC6 FC4 FC2 C2 C4 C6 T8 TP8 CP6 CP4
+CP2 P1 P3 P5 P7 P9 PO7 PO3 O1 P2 P4 P6 P8 P10 PO8 PO4 O2 Iz Oz POz Pz
+Fz AFz Fpz CPz Cz FCz""".split()
 
 # %%
 # Many experiments aggregate electrodes into regions of interest (ROIs), which we could
@@ -65,9 +68,9 @@ P4 P6 P8 P10 PO8 PO4 O2 Iz Oz POz Pz Fz AFz Fpz CPz Cz FCz""".split()
 coords = get_elec_coords(elec_names=chans)
 
 # %%
-# Now we specifiy individual colors using the `scatter_kwargs` argument. We create a
+# Now we specifiy individual colors using the ``scatter_kwargs``` argument. We create a
 # list of 64 colors corresponding to our 64 coordinates (in the original order as
-# provided by `chans`):
+# provided by ``chans``):
 
 colors = (
     ["salmon"] * 14
@@ -107,5 +110,5 @@ fig
 
 # %%
 # When using these commands from an interactive Python session, try to set
-# the IPython magic `%matplotlib` or `%matplotlib qt`, which will allow you to freely
-# view the 3D plot and rotate the camera.
+# the IPython magic ``%matplotlib`` or ``%matplotlib qt``, which will allow you to
+# freely view the 3D plot and rotate the camera.
