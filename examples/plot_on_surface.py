21c1dd141d6684b1e756cff7fb4dde6444067dbb
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2024-12-07 12:14:25 +0100

    misc doc additions

--- examples/plot_on_surface.py
+++ examples/plot_on_surface.py
@@ -19,9 +19,10 @@ For more information, check out these MNE resources:
 # sphinx_gallery_thumbnail_number = 3
 
 # %%
-# Make some imports
+# We start by importing what we need.
+#
 # This example furthermore assumes that you have installed eeg_positions
-# via ``pip install eeg_positions[docs]``
+# via ``pip install eeg_positions[docs]``.
 import mne
 import numpy as np
 
@@ -33,28 +34,30 @@ subjects_dir = mne.datasets.fetch_fsaverage().parent
 
 # %%
 # Get idealized sensor positions that were computed on a sphere
-# and export as mne.DigMontage
+# and export as :class:`mne.channels.DigMontage`
 montage = get_elec_coords(as_mne_montage=True)
 
 # %%
-# For the code below we need to provide an mne.Info object.
+# For the code below we need to provide an :class:`mne.Info` object.
 # The sampling frequency does not matter so we are setting it to 1.
 info = mne.create_info(ch_names=montage.ch_names, sfreq=1, ch_types="eeg")
 info.set_montage(montage)
 
 # %%
 # As a sanity check, let's create a spherical head model first.
-# We expect a perfect fit with our idealized electrode positions.
+# We expect a perfect fit with our idealized electrode positions,
+# because they were also computed on a sphere.
 sphere = mne.make_sphere_model(r0="auto", head_radius="auto", info=info)
 
-trans = mne.Transform("head", "mri", trans=np.eye(4))  # identity transform
+# We need a transform from the coordinates of our montage to the surface.
+# Given that both are spheres, we do not need to actually transform, and
+# can just use the identity transform.
+trans = mne.Transform("head", "mri", trans=np.eye(4))
 
 fig = mne.viz.plot_alignment(
     info=info,
     trans=trans,
-    subject="fsaverage",
-    subjects_dir=subjects_dir,
-    bem=sphere,
+    bem=sphere,  # our spherical head model
     surfaces={"head": 0.8},  # alpha value
     coord_frame="head",
     eeg=["original", "projected"],
@@ -68,14 +71,21 @@ mne.viz.set_3d_view(figure=fig, azimuth=65, elevation=75)
 # %%
 # As can be seen above, the fit of our idealized sensor positions with a spherical head
 # model is perfect.
+#
 # Now let's proceed to plot our positions on the fsaverage head.
+# We expect this to be potentially problematic, because we computed our positions
+# on a sphere ... and a sphere is usually a poor approximation of a human head.
 
+# instead of the identity transform as above, we are using the inbuilt transformation
+# from MNE-Python that can transform a Montage to fit fsaverage.
+# Note that this only works for "fsaverage" and not other surfaces.
 trans = "fsaverage"
+
 fig = mne.viz.plot_alignment(
     info=info,
     trans=trans,
-    subject="fsaverage",
-    subjects_dir=subjects_dir,
+    subject="fsaverage",  # this is fsaverage
+    subjects_dir=subjects_dir,  # directory of fsaverage
     surfaces={"head": 0.8},  # alpha value
     coord_frame="head",
     eeg=["original", "projected"],
@@ -86,14 +96,28 @@ fig = mne.viz.plot_alignment(
 
 mne.viz.set_3d_view(figure=fig, azimuth=135, elevation=80)
 
-
 # %%
-# get an automatic transformation
+# As expected, the fit above is very poor.
+#
+# The electrode positions that were computed on a sphere are not easily projected
+# to the fsaverage head.
+#
+# However, we can try to "scale" fsaverage in three dimensions to make the fit
+# slightly better.
+#
+# Below we will try to scale fsaverage to minimize the distance of sensors to the scalp
+# surface, while still trying to have the landmarks (LPA, RPA, NAS) aligned between MRI
+# and montage.
+
 fiducials = "estimated"  # taken from fsaverage
 subject = "fsaverage"
+
 coreg = mne.coreg.Coregistration(info, subject, subjects_dir, fiducials=fiducials)
-coreg.set_scale_mode("3-axis")
+
+coreg.set_scale_mode("3-axis")  # can also be "uniform", but the fit would be worse
+
 coreg.fit_fiducials(verbose=True)
+
 coreg.fit_icp(
     n_iterations=40,
     lpa_weight=1.0,
@@ -106,10 +130,9 @@ coreg.fit_icp(
 )
 trans = coreg.trans
 
-
 fig = mne.viz.plot_alignment(
     info=info,
-    trans=trans,
+    trans=trans,  # this is the optimized transform based on scaling fsaverage
     subject="fsaverage",
     subjects_dir=subjects_dir,
     surfaces={"head": 0.8},  # alpha value
@@ -122,5 +145,9 @@ fig = mne.viz.plot_alignment(
 
 mne.viz.set_3d_view(figure=fig, azimuth=135, elevation=80)
 
-
 # %%
+# Luckily, the fit is now slightly better.
+#
+# It is still not perfect, so for actual source reconstruction, you should use sensor
+# positions that are measured (digitized), rather than computed on a sphere
+# (idealized).
